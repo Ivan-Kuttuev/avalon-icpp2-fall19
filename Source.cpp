c90295347bfe831cc6f23c99e5cf8bdba7be2cf9
Ivan-Kuttuev <47279931+Ivan-Kuttuev@users.noreply.github.com>
2019-09-21 21:40:57 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	cout << "Hello world!" << endl;
+	int N;
+	cin >> N;
+
+	long long num;
+	int endofnum = 0;
+	int s = 1;
+
+
+	
+	cin >> num;
+	for (int j = 0; j < N; j++)
+	{
+		endofnum = num / 1'000'000'000'000;
+
+		for (long long i = 1'000'000'000'000; i > 10; i /= 10)
+		{
+			
+				if (s % 2 == 1)
+				{
+					endofnum = endofnum + (num % i / (i / 10) * 3 );
+				}
+				else
+				{
+					endofnum = endofnum + num % i / (i / 10) ;
+				}
+				s++;
+				
+		}
+		endofnum = endofnum % 10;
+		endofnum = 10 - endofnum;
+		if (endofnum == num % 10)
+		{
+			cout << "valid" << endl;
+		}
+		else
+		{
+			cout << "invalid" << endl;
+		}
+	}
+	
+
+	
+	
 }
