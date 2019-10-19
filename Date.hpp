befec6dfdd65f15a4b7e9ff7a761761cc834d97e
Ivan-Kuttuev <47279931+Ivan-Kuttuev@users.noreply.github.com>
2019-10-19 21:35:09 +0300

    Add files via upload

--- Date.hpp
+++ Date.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #pragma once
-
+#include <iostream>
+#include <string>
 namespace ext
 {
 	enum class Month
@@ -57,43 +58,146 @@ namespace ext
 	*/
 	TimeDelta countJND(Date date)
 	{
-		
 
-	
-		
-			int a = (14 - date.day )/ 12;
-			int y = date.year + 4800 - a;
-			int m = static_cast<int>(date.month) + 12 * a - 3;
-			int result = date.day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
 
-			return TimeDelta{ result };
+
+
+		int a = (14 - date.day) / 12;
+		int y = date.year + 4800 - a;
+		int m = static_cast<int>(date.month) + 12 * a - 3;
+		int result = date.day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+
+		return TimeDelta{ result };
 	}
 
 	/*
 		Рассчитывает количество дней между двумя датами.
 		При реализвации используйте CountJND
 	*/
-	TimeDelta countDistance(Date from, Date to);
+	TimeDelta countDistance(Date from, Date to)
+	{
+		int a1 = (14 - from.day) / 12;
+		int y1 = from.year + 4800 - a1;
+		int m1 = static_cast<int>(from.month) + 12 * a1 - 3;
+		int result1 = from.day + (153 * m1 + 2) / 5 + 365 * y1 + y1 / 4 - y1 / 100 + y1 / 400 - 32045;
 
-	
+		int a2 = (14 - to.day) / 12;
+		int y2 = to.year + 4800 - a2;
+		int m2 = static_cast<int>(to.month) + 12 * a2 - 3;
+		int result2 = to.day + (153 * m2 + 2) / 5 + 365 * y2 + y2 / 4 - y2 / 100 + y2 / 400 - 32045;
+
+		int dist = result2 - result1;
+
+		return TimeDelta{ dist };
+	}
+
+	std::string monthToString(Month month)
+	{
+		switch (month)
+		{
+		case Month::January:
+			return "january ";			
+		case Month::February:
+			return "february ";
+		case Month::March:
+			return "march ";
+		case Month::April:
+			return "april ";			
+		case Month::May:
+			return "may ";
+		case Month::June:
+			return "june ";
+		case Month::July:
+			return "july ";
+		case Month::August:
+			return"august ";			
+		case Month::September:
+			return "september ";			
+		case Month::October:
+			return "october ";
+		case Month::Novemver:
+			return "november ";
+		case Month::December:
+			return  "december ";			
+		default:
+			return "";
+		}
+	}
+
 	/*
 		Выводит в консоль
 	*/
-	void print(Date data, DateFormat format = DateFormat::MonthAsInt);
-	void print(Month month, DateFormat format = DateFormat::MonthAsInt);
+
+	void print(Month month, DateFormat format = DateFormat::MonthAsInt)
+	{
+		if (format == DateFormat::MonthAsInt)
+		{
+			std::cout << static_cast<int>(month) << " ";
+		}
+
+		else if (format == DateFormat::MonthAsString)
+		{
+			std::cout << monthToString(month) << " ";
+		}
+	}
+
+	void print(Date data, DateFormat format = DateFormat::MonthAsInt)
+	{
+		std::cout << data.year << " ";
+
+		print(data.month, format);
+
+		std::cout << data.day;
+	}	
+
 	void print(TimeDelta delta);
+
+
+
 
 	/*
 		Возвращает сезон (зима, весна, лето, осень) передаваемой даты
 	*/
-	Season getSeason(Date date);
-	Season getSeason(Month month);
+	Season getSeason(Date date)
+	{
+		
+		if (static_cast<int>(date.month) < 3 || static_cast<int>(date.month) == 12)
+		{
+			return Season::Winter;
+		}
+		else if (static_cast<int>(date.month) > 2 && static_cast<int>(date.month) < 6)
+		{
+			return Season::Spring;
+		}
+		else if (static_cast<int>(date.month) > 5 && static_cast<int>(date.month) < 9)
+		{
+			return Season::Summer;
+		}
+		else 
+		{
+			return Season::Autumn;
+		}
+		
+	}
+	Season getSeason(Month month)
+	{
+		getSeason(month);		
+	}
 
 	/*
 		Написать перегрузку для следующих логических операторов
 	*/
-	bool operator == (const Date lhs, const Date rhs);
-	bool operator != (const Date lhs, const Date rhs);
+	bool operator == (const Date lhs, const Date rhs)
+	{
+		return lhs.year == rhs.year
+			&& lhs.month == rhs.month
+			&& lhs.day == rhs.day;
+	}
+	bool operator != (const Date lhs, const Date rhs)
+	{
+		return !(lhs == rhs);
+
+	}
 	bool operator < (const Date lhs, const Date rhs);
 	bool operator <= (const Date lhs, const Date rhs);
 	bool operator > (const Date lhs, const Date rhs);
--- Source.cpp
+++ Source.cpp
@@ -6,14 +6,41 @@ using namespace ext;
 
 int main()
 {
-	Date datefrom{ 2018,Month::December,1 };
-	//Date dateto{ 10,Month::September,221 };
+	Date date{ 2018,Month::December,1 };
+	Date dateto{ 2020,Month::September,4 };
 
-	TimeDelta delta = countJND(datefrom);
-	//TimeDelta delta = countJND(dateto);
+	TimeDelta delta1 = countJND(date);
+	
 
-	cout << countJND(datefrom).delta;
+	cout << delta1.delta << endl;
 
+	TimeDelta distance = countDistance(date, dateto);
+	
+	cout << distance.delta << endl;
+
+	print(date, DateFormat::MonthAsInt); //2018 12 1
+	cout << endl;
+
+	print(date, DateFormat::MonthAsString); //2018 december 1
+	cout << endl;
+
+
+
+
+	Month month = Month::May;
+
+	print(month, DateFormat::MonthAsInt);  // 5
+	cout << endl;
+
+	print(month, DateFormat::MonthAsString);  // may
+	cout << endl;
+
+	Date first = { 2003 , Month::May , 5 };
+	Date second = { 2003 , Month::May , 5 };
+	Date third = { 2004 , Month::April , 5 };
+
+	cout << (first == second) << endl; //true
+	cout << (second == third) << endl; //false
 
 	
 }
